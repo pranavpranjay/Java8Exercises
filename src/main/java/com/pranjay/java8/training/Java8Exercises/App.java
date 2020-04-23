b660a9adb0a729578f9a661ad988be79d808e729
pranavpranjay <pranavpranjay@gmail.com>
2020-04-23 15:49:17 +0530

    Project Created...

---
+++ src/main/java/com/pranjay/java8/training/Java8Exercises/App.java
@@ -0,0 +1,13 @@
+package com.pranjay.java8.training.Java8Exercises;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
